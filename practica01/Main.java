2034ee3d2ea019a0410ec36e429ae3a028b167cf
carlosandradeh <54073903+carlosandradeh@users.noreply.github.com>
2019-08-17 22:39:17 -0500

    Practica 1 Hicimos nuestro primer programa en Java
    Programa con especificaciones del sistema

---
+++ practica01/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[]args){
+        System.out.println("openjdk 11.0.4 2019-07-16");
+        System.out.println("OpenJDK Runtime Environment (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3)");
+        System.out.println("OpenJDK 64-Bit Server VM (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3, mixed mode, sharing");
+    }
+}
